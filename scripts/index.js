1ee73a99c4abcd8e878d1cb1c2adcf47a534af61
AleksKovalev <aleks.kovalev@mail.ru.ru>
2024-05-17 13:20:39 +0300

    v1

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,26 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
 
 // @todo: DOM узлы
+const cardsContainer = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
+function createCard(initialCard, deletteCard) {
+  const card = cardTemplate.querySelector(".card").cloneNode(true);
+  card.querySelector(".card__image").src = initialCard.link;
+  card.querySelector(".card__title").textContent = initialCard.name;
+  deleteButton = card.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", () => deletteCard(card));
+
+  return card;
+}
 
 // @todo: Функция удаления карточки
+deletteCard = function (card) {
+  card.remove();
+};
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach((initialCard) => {
+  cardsContainer.append(createCard(initialCard, deletteCard));
+});
